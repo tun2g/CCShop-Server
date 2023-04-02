417d4182fd5642938f2ddf3435be79a642d395ad
tun2g <tunghcltt@gmail.com>
2023-04-02 15:54:27 +0700

    notification of message

--- Services/ioSocket.js
+++ Services/ioSocket.js
@@ -27,8 +27,22 @@ module.exports = (server) => {
         console.log(socket.id," leave room ", key)
         socket.leave(key)
     })
-
+    socket.on("notifyMessage",async(data)=>{
+        //gửi đến thông báo
+        const name=data.name
+        const str=await redis.get(data.receiver)
+        const listNotifications=JSON.parse(str)?JSON.parse(str):[]
+        listNotifications.push({
+            name:name,
+            sender:data.sender,
+            type:"message",
+            avatar:data.avatar
+        })
+        socket.nsp.in(data.receiver).emit("notifyMessage",{listNotifications})
+        redis.set(data.receiver,JSON.stringify(listNotifications))
+    })
     socket.on("message", async(data)=>{
+        // gửi đến chat
         const room=roomFormat(data.sender,data.receiver)
         const string= await redis.get(room)
         const listMessages=JSON.parse(string)?JSON.parse(string):[]
@@ -37,7 +51,6 @@ module.exports = (server) => {
             sender:data.sender,
             receiver:data.receiver,
         }) 
-        
         socket.nsp.in(room).emit("message",{listMessages})
         redis.set(room,JSON.stringify(listMessages))
     })
